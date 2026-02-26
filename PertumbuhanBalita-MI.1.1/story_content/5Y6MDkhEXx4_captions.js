b4a1b437b578dddc124e3a7552ef5eb526a4151d
Balai Pelatihan Kesehatan Batam <bapelkesbatam1@gmail.com>
2026-02-26 21:34:59 +0700

    Add files via upload

---
+++ PertumbuhanBalita-MI.1.1/story_content/5Y6MDkhEXx4_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/5Y6MDkhEXx4_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.102.35072.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.315%0D%0ABerikut%20adalah%20fungsi%20Pemantauan%20Pertumbuhan%20Balita.%0D%0A%0D%0A"}]})
